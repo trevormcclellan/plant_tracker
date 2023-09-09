928af0546bf335bdee38cd0994d799c970cb9e67
Trevor McClellan <tsmcclel@byu.edu>
2023-09-09 17:46:53 -0600

    Updated pwa config for icons

--- app/vue.config.js
+++ app/vue.config.js
@@ -2,6 +2,12 @@ module.exports = {
     // ...Other Vue config options here, possibly
     pwa: {
       name: "Plants!",
-      themeColor: "#198754"
+      themeColor: "#198754",
+
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
     }
   };
