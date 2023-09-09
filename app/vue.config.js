01349357e71cac83eb59e20bfdfc36eeebfe927c
Trevor McClellan <tsmcclel@byu.edu>
2023-09-09 16:29:14 -0600

    PWA metadata

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,17 +1,7 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true,
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/c/'
-//     : '/',
-//   outputDir: process.env.NODE_ENV === 'production'
-//     ? './../DnsForItLearningLabs/c'
-//     : 'dist',
-//   pages: {
-//     index: "./src/main.js",
-//     // This generates a login.html page which works on the Azure Functions host and
-//     // on GitHub pages or any other static server that defaults to a .html extension
-//     // when no extension is in the URL.
-//     login: "./src/main.js"
-//   }
-// })
+module.exports = {
+    // ...Other Vue config options here, possibly
+    pwa: {
+      name: "Plants!",
+      themeColor: "#198754"
+    }
+  };
